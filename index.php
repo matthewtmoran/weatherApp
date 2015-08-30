26cf2b9e0a559634fb226b625d743e6cb4b08876
Matt Moran <mmoran@locallogicmedia.com>
2015-08-30 14:54:57 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+<div id="weatherApp">
+    <h1>Weather app</h1>
+    <div class="info-container main-info">
+        <div class="iconImage"></div>
+        <div class="degrees"></div>
+    </div>
+    <div class="info-container sub-info">
+        <div class="location"></div>
+        <div class="description"></div>
+        <div class="wind">
+            <div class="direction"></div>
+            <div class="speed"></div>
+        </div>
+    </div>
+        <button class="sysToggle">Switch to Metric</button>
+</div>
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+
+    $.getJSON("http://ipinfo.io", function(response) {
+        console.log(response.city, response.region, response.country);
+
+        var degree = "F";
+        var tempImpMet = "F";
+        var backgroundImage;
+
+
+        getWeatherInfo();
+        function getWeatherInfo() {
+
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + response.city +"," + response.region + "&units=imperial", function(weatherResponse) {
+                    console.log(weatherResponse);
+
+                //data to variables
+                var getDesc = weatherResponse.weather[0].description,
+                    currentLocation = response.city +", " + response.region,
+                    deg = weatherResponse.wind.deg,
+                    windSpeed = weatherResponse.wind.speed,
+                    currentTemp = weatherResponse.main.temp.toFixed(),
+                    iconID = weatherResponse.weather[0].icon,
+                    iconImageURL = "http://openweathermap.org/img/w/" + iconID + ".png";
+                //dom locaiton to variables
+                var weatherIcon = $('.iconImage'),
+                    weatherDegree = $('.degrees'),
+                    weatherLocation = $('.location'),
+                    weatherDescription = $('.description'),
+                    weatherSpeed = $('.speed'),
+                    weatherDirection = $('.direction');
+
+
+                //append data to dom
+                weatherIcon.append('<p class="weatherData"><img src="' + iconImageURL + '"/></p>').hide().fadeIn();
+                weatherDegree.append('<p class="weatherData" id="tempDetails"><span id="tempChange"> ' + currentTemp + ' <span id="tempID"> ' + tempImpMet + ' </span></p>');
+
+                weatherLocation.append('<p class="weatherData">' + currentLocation + '</p>').hide().fadeIn();
+                weatherDescription.append('<p class="weatherData">' + getDesc + '</p>').hide().fadeIn();
+                weatherSpeed.append('<p class="weatherData">' + windSpeed + '<span class="windID"> MPH</span></p>').hide().fadeIn();
+                weatherDirection.append('<p class="weatherData">' + windDirection(deg) + '</p>').hide().fadeIn();
+                //set background image
+                function setBackground() {
+                    switch(currentTemp) {
+                        case currentTemp <= 35:
+                            backgroundImage = "cold.jpeg";
+                            break;
+                        case currentTemp > 35 && currentTemp < 80:
+                            backgroundImage = "medium.jpeg";
+                            break;
+                        case currentTemp >= 80:
+                            backgroundImage = "hot.jpeg";
+                            break;
+                        default:
+                            backgroundImage = "medium.jpeg";
+                    }
+                    $('body').css({'background': 'url(img/bkg/' + backgroundImage});
+                } setBackground();
+
+                //calc metric/imperial conversion
+                $('.sysToggle').on('click', function() {
+                    if (degree === "F") {
+                        degree = "C";
+                        $('.sysToggle').text('Switch to Fahrenheit ');
+                        currentTemp = (currentTemp - 32) * 5 / 9;
+                        updateDegree(degree, currentTemp.toFixed());
+                    } else if (degree === "C") {
+                        degree = "F";
+                        $('.sysToggle').text('Switch to Celsius');
+                        currentTemp = currentTemp * 9 / 5 + 32;
+                        updateDegree(degree, currentTemp);
+                    }
+                });
+                //write conversion after calc
+                function updateDegree(deg, temp) {
+                    $('#tempChange').fadeOut("normal", function(){
+                        $(this).remove();
+                    });
+                    $('#tempID').fadeOut("normal", function(){
+                        $(this).remove();
+                    });
+
+                    $('#tempDetails').html('<span id="tempChang"> ' + temp + ' </span><span id="tempID">' + deg + '</span>').hide().fadeIn();
+                }
+
+                //calc wind direction
+                function windDirection(deg) {
+                    var compassArray = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+                    var direction = Math.floor(deg / 45);
+                    return compassArray[direction];
+                }
+
+            }, "jsonp");
+        }
+    }, "jsonp");
+});
+
+
